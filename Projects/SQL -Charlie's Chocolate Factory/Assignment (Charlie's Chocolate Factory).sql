09752491f1e4af4c4e823c62ac83eb74c245f151
Jack1634 <muslu34613461@gmail.com>
2021-11-12 19:12:46 +0300

    SQL -Charlie's Chocolate Factory

---
+++ Projects/SQL -Charlie's Chocolate Factory/Assignment (Charlie's Chocolate Factory).sql
@@ -0,0 +1,21 @@
+
+Charlie's Chocolate Factory company produces chocolates. The following product information is stored: product name, product ID, and quantity on hand. These chocolates are made up of many components. Each component can be supplied by one or more suppliers. The following component information is kept: component ID, name, description, quantity on hand, suppliers who supply them, when and how much they supplied, and products in which they are used. On the other hand following supplier information is stored: supplier ID, name, and activation status.
+
+Assumptions
+
+A supplier can exist without providing components.
+A component does not have to be associated with a supplier. It may already have been in the inventory.
+A component does not have to be associated with a product. Not all components are used in products.
+A product cannot exist without components.
+
+Create an ERD to show how you would track this information. (You can use draw.io)
+
+Show entity names, primary keys, attributes for each entity, and relationship types between the entities. 
+
+Do the following exercises, using the data model you designed before.
+
+     a) Create a database named "Manufacturer"
+
+     b) Create the tables in the database.
+
+     c) Define table constraints.
