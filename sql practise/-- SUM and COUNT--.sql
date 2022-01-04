dbfed748d5f8ec2d2cdddc18a2b54c52167132f8
Jack1634 <muslu34613461@gmail.com>
2022-01-04 06:58:19 +0300

    Sql

---
+++ sql practise/-- SUM and COUNT--.sql
@@ -0,0 +1,43 @@
+				-- SUM and COUNT--
+
+--1. Show the total population of the world.
+	-- world(name, continent, area, population, gdp)
+
+	SELECT SUM(population) FROM world
+
+--2. List all the continents - just once each.
+
+	SELECT DÝSTÝNCT continent FROM world
+
+--3. Give the total GDP of Africa
+
+	SELECT sum(gdp) FROM world
+    WHERE continent='Africa'
+
+--4. How many countries have an area of at least 1000000
+
+	 SELECT COUNT(NAME) FROM world
+	 WHERE area>1000000
+
+--5. What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+
+     SELECT SUM(population) from world
+	 where name in ('Estonia', 'Latvia', 'Lithuania')
+
+--6. For each continent show the continent and number of countries.
+
+     SELECT continent , COUNT(name) FROM world
+	 GROUP BY continent
+
+--7. For each continent show the continent and number of countries with populations of at least 10 million.
+
+     SELECT continent, COUNT(name) FROM world
+	 WHERE population >=10000000
+	 GROUP BY continent
+
+--8. List the continents that have a total population of at least 100 million.
+
+	 SELECT  continent 
+	 FROM world
+	 GROUP BY continent
+	 HAVING sum(population) >= 100000000
